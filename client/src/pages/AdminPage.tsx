34a8341f09fe075549284beaa4d52de88ab900b8
abdelmouiz <a_youb@estin.dz>
2025-07-01 14:36:32 -1100

    Crud api in progress the first is the client side

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 function AdminPage() {
   return (
-    <div>
-        <h1>Admin page</h1>
-        <p>Welcome to the admin page!</p>
-        <Link to="/client" className="text-blue-500 hover:underline">
-          Go to Client Page
-          </Link>
+    <div className='flex justify-center items-center w-full h-full flex-col'>
+      <div className='flex justify-center items-center flex-col w-full'>
+          <h1>Admin page</h1>
+      </div>
+      <div className='w-full h-full grid grid-cols-5 grid-rows-10 '>
+        <form  className='col-start-2 col-end-5 row-start-2 row-end-10 border-[1px]'></form>
+      </div>
     </div>
   )
 }
